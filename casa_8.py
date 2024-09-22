e2853d57edd04d2f262ac036813813d3feb43f0c
gjlopez2 <gjlopez2@miners.utep.edu>
2024-09-22 15:30:04 -0600

    Finished practice
    Practiced all iterations in the video

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,46 @@
+import pandas as pd
 
+
+df = pd.read_csv('big-mac-full-index.csv')
+
+print(df.head())
+
+#index method
+
+print(df.index)
+''''
+for index in df.index:
+    print(df['date'][index],df[])
+    '''
+#using loc
+
+#.index accessing each row (0, 1, 2)
+'''
+for index in range(len(df)):
+    print(df.loc[index, 'name'])
+'''
+#iloc
+"""
+
+for index in range(len(df)):
+    print(df.iloc[index, 0], df.iloc[index, 1])
+
+"""
+
+''''
+
+#Using Iterrows r is row
+for i,r in df.iterrows():
+    print(r,['date'],
+          r['name'])
+'''
+#itertuples
+'''
+
+for row in df.itertuples():
+    print(getattr(row, 'date'),
+      getattr(row, 'name'))
+'''
+#Using APPLY method
+
+print(df.apply(lambda row: row['name'], axis = 1))
